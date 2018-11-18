4711f9f7e0358f2702762cb397965b5cf343e63c
saileifeng <spring4plum@gmail.com>
2018-11-18 18:12:08 +0800

    update jieqi day

---
+++ calendar/ganzhi_test.go
@@ -0,0 +1,30 @@
+package calendar
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestGanZhi(t *testing.T) {
+	solarNow := NewSolarNow()
+	fmt.Println(solarNow)
+	lunarNow := NewLunarNow()
+	fmt.Println(lunarNow)
+	fmt.Println(AnimalYear(lunarNow.Year()))
+	fmt.Println(lunarNow.Year(),lunarNow.Month(),lunarNow.Day())
+	ganzhiY,ganzhiM,ganzhiD:=GanZhiYMD(solarNow.Year(),int(solarNow.Month()),solarNow.Day())
+	fmt.Println(ganzhiY,ganzhiM,ganzhiD)
+	JieQiDay(2018,11)
+	JieQiDay(2012,1)
+	JieQiDay(2012,2)
+	JieQiDay(2012,3)
+	JieQiDay(2012,4)
+	JieQiDay(2012,5)
+	JieQiDay(2012,6)
+	JieQiDay(2012,7)
+	JieQiDay(2012,8)
+	JieQiDay(2013,4)
+	JieQiDay(1993,3)
+	JieQiDay(1980,1)
+	JieQiDay(1991,5)
+}
