23f384c426302d966cdf20ea3587534aabc22167
henrylee2cn <henrylee2cn@gmail.com>
2019-06-11 21:54:03 +0800

    chore
    1. DerefType -> DereferenceType
    2. DerefValue -> DereferenceValue
    
    Change-Id: I5a41f4b3ce6d3cc853852b2c03db5a603dc72f01

--- utils.go
+++ utils.go
@@ -16,17 +16,17 @@ func InitAndGetString(strPtr *string, def string) string {
 	return *strPtr
 }
 
-// DerefType dereference, get the underlying non-pointer type.
-func DerefType(t reflect.Type) reflect.Type {
+// DereferenceType dereference, get the underlying non-pointer type.
+func DereferenceType(t reflect.Type) reflect.Type {
 	for t.Kind() == reflect.Ptr {
 		t = t.Elem()
 	}
 	return t
 }
 
-// DerefValue dereference and unpack interface,
+// DereferenceValue dereference and unpack interface,
 // get the underlying non-pointer and non-interface value.
-func DerefValue(v reflect.Value) reflect.Value {
+func DereferenceValue(v reflect.Value) reflect.Value {
 	for v.Kind() == reflect.Ptr || v.Kind() == reflect.Interface {
 		v = v.Elem()
 	}
