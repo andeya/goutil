b83f483a12190237baed44075976137625b89e58
henrylee2cn <henrylee2cn@gmail.com>
2019-06-28 11:35:24 +0800

    feat: Add some functions and optimize code
    Change-Id: Id0ddc21414c57b6640aee128cc6c4212c56dc6bb

--- string_test.go
+++ string_test.go
@@ -5,6 +5,20 @@ import (
 	"testing"
 )
 
+func TestBytesToString(t *testing.T) {
+	bb := []byte("testing: BytesToString")
+	ss := BytesToString(bb)
+	t.Logf("type: %T, value: %v", ss, ss)
+}
+
+func TestStringToBytes(t *testing.T) {
+	s := "testing: StringToBytes"
+	b := StringToBytes(s)
+	t.Logf("type: %T, value: %v, val-string: %s\n", b, b, b)
+	b = append(b, '!')
+	t.Logf("after append:\ntype: %T, value: %v, val-string: %s\n", b, b, b)
+}
+
 func TestSnakeString(t *testing.T) {
 	data := [][2]string{
 		{"XxYy", "xx_yy"},
