c9345eb136ca6a0045c484d8b22e5ebcbfc7159e
henrylee2cn <henrylee2cn@gmail.com>
2019-08-20 20:44:24 +0800

    feat: Add some functions
    Change-Id: Ifb750b3b7984e2f2a434116e4d00c934cc6f10e9

--- sets_test.go
+++ sets_test.go
@@ -36,3 +36,19 @@ func TestIntersectStrings(t *testing.T) {
 		assert.Equal(t, c.intersect, IntersectStrings(c.set1, c.set2))
 	}
 }
+
+func TestStringsToXXX(t *testing.T) {
+	var cases = []struct {
+		strs     []string
+		expected []int
+	}{
+		{[]string{"1", "", "3"}, []int{1, 0, 3}},
+	}
+	for _, c := range cases {
+		r, err := StringsToInts(c.strs)
+		assert.EqualError(t, err, "strconv.Atoi: parsing \"\": invalid syntax")
+		r, err = StringsToInts(c.strs, true)
+		assert.Nil(t, err)
+		assert.Equal(t, c.expected, r)
+	}
+}
