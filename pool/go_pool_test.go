493ba6f901f5072f409b3907f42dee25b846f43e
henrylee2cn <henrylee2cn@gmail.com>
2019-08-25 11:38:52 +0800

    feat(gopool): Add MustGo, always try to use goroutine callbacks until execution is complete or the context is canceled.
    Change-Id: I3c6a04a0af5de3d5eb00352cf885f7d131abb0c6

--- pool/go_pool_test.go
+++ pool/go_pool_test.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"context"
 	"sync"
 	"testing"
 )
@@ -27,4 +28,30 @@ func TestGoPool(t *testing.T) {
 	wg.Wait()
 	gp.Stop()
 	t.Logf("retryTimes: %d", retryTimes)
+}
+
+func TestGoPool2(t *testing.T) {
+	gp := NewGoPool(10, 0)
+	wg := new(sync.WaitGroup)
+	retryTimes := 0
+	var err error
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		a := i
+		err = gp.MustGo(context.Background(), func() {
+			t.Log("done:", a)
+			wg.Done()
+		})
+		if err != nil {
+			retryTimes++
+			i--
+			t.Log(err)
+			wg.Done()
+		}
+	}
+	wg.Wait()
+	gp.Stop()
+	if retryTimes > 0 {
+		t.Fatalf("except 0, but got %d", retryTimes)
+	}
 }
